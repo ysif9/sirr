5a217dca41fb669cf2a07a998cc06a615ca5d80c
Philo <philosbisnesacc@gmail.com>
2025-09-10 04:55:47 +0300

    feat: implement Stepper component with context and navigation functionality

---
+++ frontend/reporter/components/Stepper.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { useState } from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Stepper,
+  StepperIndicator,
+  StepperItem,
+  StepperSeparator,
+  StepperTrigger,
+} from "@/components/ui/stepper"
+
+const steps = [1, 2, 3, 4]
+
+export default function Component() {
+  const [currentStep, setCurrentStep] = useState(2)
+
+  return (
+    <div className="mx-auto max-w-xl space-y-8 text-center">
+      <Stepper value={currentStep} onValueChange={setCurrentStep}>
+        {steps.map((step) => (
+          <StepperItem key={step} step={step} className="not-last:flex-1">
+            <StepperTrigger asChild>
+              <StepperIndicator />
+            </StepperTrigger>
+            {step < steps.length && <StepperSeparator />}
+          </StepperItem>
+        ))}
+      </Stepper>
+      <div className="flex justify-center space-x-4">
+        <Button
+          variant="outline"
+          className="w-32"
+          onClick={() => setCurrentStep((prev) => prev - 1)}
+          disabled={currentStep === 1}
+        >
+          Prev step
+        </Button>
+        <Button
+          variant="outline"
+          className="w-32"
+          onClick={() => setCurrentStep((prev) => prev + 1)}
+          disabled={currentStep > steps.length}
+        >
+          Next step
+        </Button>
+      </div>
+    </div>
+  )
+}
