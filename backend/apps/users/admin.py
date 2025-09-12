4fffe293e30b4e87d05cfe8c33e8d0c03ffcd8c1
Yousif Abdulhafiz <22P0232@eng.asu.edu.eg>
2025-09-12 03:38:00 +0300

    feat(backend): customize user model and register in admin
    - Extended the user model to include a `role` field with admin and investigator roles
    - Added properties (`is_admin` and `is_investigator`) for role validation
    - Registered the customized user model in the Django admin panel

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,2 +1,10 @@
+from apps.users.models import User
+
+from django.contrib import admin
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """User admin registration."""
+    pass
